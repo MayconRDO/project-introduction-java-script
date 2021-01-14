c2f701fba9a17a6c7f9d254cfefb6696e2c91ef3
Maycon Roberto de Oliveira <maycon.roberto.oliveira@gmail.com>
2021-01-14 20:04:21 -0300

    My first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// O código abaixo escreve uma mensagem no console devtools
+// console.log('Bem vindos');
+
+// Objeto
+// console.log({
+//     name: "Maycon",
+//     idade: 32,
+//     andar: function(){
+//         console.log('andar');
+//     }
+// });

285b85ecce4143d02b27a2fc77f6faa6826bc3e7
Maycon Roberto de Oliveira <maycon.roberto.oliveira@gmail.com>
2021-01-20 20:25:09 -0300

    Arrays and functions

---
+++ functions.js
@@ -0,0 +1,38 @@
+
+// // Aplicativo de frases motivacionais
+
+// // Declaração da função
+// function createPhrases(){
+//     console.log('Estudar é muito bom')
+//     console.log('Paciência é persistência')
+//     console.log('Revisão é mão do aprendizado')
+// }
+
+// // Executar a função
+// // Rodar a função, chamar, invocar
+// // execute, run, call, invoke
+// createPhrases();
+
+// console.log('Fim do programa')
+
+// Function expression ou function anonymous    
+// const sum = function (number1, number2){
+//     return number1 + number2;
+// }
+
+// console.log( sum(2, 3))
+
+// Function hoisting
+
+// sayMyName();
+
+// function sayMyName(){
+//     console.log('Maycon');
+// }
+
+// Arrow function
+// const sayMyName = () => {
+//     console.log('Maycon');
+// }
+
+// callback function
--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // O código abaixo escreve uma mensagem no console devtools
 // console.log('Bem vindos');
 
-// Objeto
+// Tipo de dado: Objeto
 // console.log({
 //     name: "Maycon",
 //     idade: 32,
@@ -9,3 +9,72 @@
 //         console.log('andar');
 //     }
 // });
+
+// Tipo de dado: Array
+
+// let name = "Maycon", isHuman = true, age = 32;
+
+// console.log(name, isHuman, age);
+// console.log(typeof name);
+// console.log(typeof isHuman);
+// console.log(typeof age);
+
+// Interpolação
+// console.log(`O ${name} tem ${age} anos`);
+
+// Objects
+// const person = {
+//     name: 'Maycon',
+//     age: 32,
+//     isAdmin: true
+// }
+
+// console.log(`O ${person.name} tem ${person.age} anos`);
+
+// Arrays
+// const animals = [
+//     'Lion',
+//     'Monkey',
+//     'Cat'
+// ]
+
+// console.log(animals[0]);
+
+let weight;
+
+console.log(typeof weight);
+
+let name = 'Maycon', age = 32, stars = 4.5, isSubcribe = true;
+
+let student = {
+    name: 'Maycon', age: 32, weight: 84.50, isSubcribe: true
+}
+
+console.log(student);
+
+console.log(`${student.name}, de ${student.age} anos pesa ${student.weight}`)
+
+let students = [];
+
+students = [
+    student
+]
+
+console.log(students)
+
+console.log(students[0])
+
+const john = {
+    name: 'Eloisa', age: 4, weight: 13.5, isSubcribe: false
+}
+
+students = [
+    student,
+    john
+]
+
+console.log(students);
+
+var a
+console.log(a)
+a = 1
